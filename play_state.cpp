770deff0182a1aa65f4e9ee36d1f7fae90588cb8
zyenapz <zyenapz@gmail.com>
2021-06-14 19:30:01 +0800

    It's done, I guess.

--- play_state.cpp
+++ play_state.cpp
@@ -1,12 +1,54 @@
 #include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <bits/stdc++.h>
 #include "context.h"
 #include "menu_state.h"
 #include "play_state.h"
 
-PlayState::PlayState(Context* context) {
+PlayState::PlayState(Context *context)
+{
     m_Context = context;
+
+    // Initialize currentQuestion and answer
+    currentQuestion = 0;
+    score = 0;
+    answer = "";
+
+    // Shuffle words
+    std::srand(std::time(0));
+    random_shuffle(words.begin(), words.end());
 }
 
-void PlayState::Handle() {
-    std::cout << "There are 10 words to guess. Get ready!\n";
+void PlayState::Handle()
+{
+    if (currentQuestion < words.size())
+    {
+        // Display question and get answer
+        std::cout << currentQuestion + 1 << ". What is " << words[currentQuestion][1] << "?\n";
+        std::cout << "> Answer: ";
+        std::cin >> answer;
+
+        // transform answer to lowercase
+        transform(answer.begin(), answer.end(), answer.begin(), ::tolower);
+
+        // Check answer
+        if (answer == words[currentQuestion][0])
+        {
+            std::cout << "Correct!";
+            score++;
+        }
+        else {
+            std::cout << "Wrong!";
+        }
+
+        // Increment answer
+        currentQuestion++;
+    }
+    else {
+        std::cout << "Game over!" << "\n";
+        std::cout << "Your correctly guessed: " << score << " out of " << words.size() << " words.\n";
+        m_Context->SetState(new MenuState(m_Context));
+    }
 }
--- play_state.h
+++ play_state.h
@@ -1,12 +1,26 @@
 #include "context.h"
+#include <vector>
+#include <string>
+#include <iterator>
+using namespace std;
 // -------------------
 #pragma once
 class PlayState : public State
 {
     private:
-        Context *m_Context;
+        Context* m_Context;
+
+        vector< vector<string> > words = {
+            {"banana", "a long yellow fruit"},
+            {"apple", "a round red fruit"},
+            {"book", "a collection of pages bound together with covers"}
+        };
+        vector< vector<string> >::const_iterator iter;
+        int currentQuestion;
+        int score;
+        string answer;
 
     public:
-        PlayState(Context *context);
+        PlayState(Context* context);
         void Handle();
 };
